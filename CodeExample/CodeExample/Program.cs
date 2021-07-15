3962fb5ff91ee9508ad68ee39dac7c0e18309d60
peachy-1 <55264958+peachy-1@users.noreply.github.com>
2021-07-14 22:37:25 -0400

    commit initial

---
+++ CodeExample/CodeExample/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RainFall
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] rain = new double[12];
+
+            for (int i = 0; i < 12; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"Please enter the amount of rain for month {i+1}");
+
+                    rain[i] = Convert.ToDouble(Console.ReadLine());
+
+                    Console.WriteLine();
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine(e);
+
+                    i -= 1;
+                }
+            }
+
+            RainFall rainfall = new RainFall(rain);
+
+            Console.WriteLine($"Total rainfall:   {rainfall.TotalRain()}");
+            Console.WriteLine($"Average rainfall: {rainfall.AvgRain()}");
+            Console.WriteLine($"Highest rainfall: {rainfall.MostRain()}");
+            Console.WriteLine($"Lowest rainfall:  {rainfall.LeastRain()}");
+        }
+    }
+}
---
+++ CodeExample/CodeExample/RainFall.cs
@@ -0,0 +1,78 @@
+﻿using System.Linq;
+
+namespace RainFall
+{   
+    /// <summary>
+    /// class for rainfall
+    /// </summary>
+    class RainFall
+    {
+        private double[] yearRain;
+        
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="stuff"></param>
+        public RainFall(double[] stuff)
+        {
+            this.yearRain = stuff;
+        }
+        
+        /// <summary>
+        /// returns the sum of all attributes of the array
+        /// </summary>
+        /// <returns>array sum</returns>
+        public double TotalRain()
+        {
+            double total = 0;
+
+            foreach (double month in yearRain)
+            {
+                total += month;
+            }
+
+            return total;
+        }
+        
+        /// <summary>
+        /// returns the average rainfall for the entirety of the array
+        /// </summary>
+        /// <returns>array average</returns>
+        public double AvgRain()
+        {
+            double total = 0;
+            double avg;
+
+            foreach (double month in yearRain)
+            {
+                total += month;
+            }
+
+            avg = total / yearRain.Length;
+
+            return avg;
+        }
+        
+        /// <summary>
+        /// returns the value with the highest recoreded rainfall
+        /// </summary>
+        /// <returns>highest decimal</returns>
+        public double MostRain()
+        {
+            double high = yearRain.Max();
+
+            return high;
+        }
+        
+        /// <summary>
+        /// Returns the value with the lowest recorded rainfall
+        /// </summary>
+        /// <returns>lowest decimal</returns>
+        public double LeastRain()
+        {
+            double low = yearRain.Min();
+
+            return low;
+        }
+    }
+}
